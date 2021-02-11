4cd83610e8a0578904df2f7879020e7b7320b5d9
msanchcr <miansacre+dev@gmail.com>
2021-02-11 10:40:45 +0100

    Open commands with Ctrl + Shift + P and search for 'Transform to Uppercase'.

--- src/app/02-multi-cursor-edicion/05-capitalizar.ts
+++ src/app/02-multi-cursor-edicion/05-capitalizar.ts
@@ -20,21 +20,21 @@
 
 function capitalizados() {
 
-    const hulk       = 'brouce banner';
-    const Hawkeye    = 'cinton francis';
-    const ironman    = 'tony stark';
-    const spiderman  = 'peter parker';
-    const viudaNegra = 'natalia romanova';
+    const HULK       = 'Brouce Banner';
+    const HAWKEYE    = 'Cinton Francis';
+    const IRONMAN    = 'Tony Stark';
+    const SPIDERMAN  = 'Peter Parker';
+    const VIUDANEGRA = 'Natalia Romanova';
 
 }
 
 function minusculas() {
 
-    const HULK       = 'brouce banner';
-    const HAWKEYE    = 'cinton francis';
-    const IRONMAN    = 'tony stark';
-    const SPIDERMAN  = 'peter parker';
-    const VIUDANEGRA = 'natalia romanova';
+    const hulk       = 'brouce banner';
+    const hawkeye    = 'cinton francis';
+    const ironman    = 'tony stark';
+    const spiderman  = 'peter parker';
+    const viudanegra = 'natalia romanova';
 
 }
 
